ffabcabf275109ae185d26588492c61df75a4a7d
ZAREK ALEXIS NAZAR DUTKA <47696115@ort>
2024-06-05 12:01:09 -0300

    terminado

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@
 import './App.css';
 import UserList from "./components/Users.jsx"
 function App() {
+  
   return (
+    
     <div className="App">
       <UserList></UserList>
     </div>
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,27 +1,78 @@
-import { useEffect, useState } from "react"
- import "./Users.css"
+import { useEffect, useState } from "react";
+import "./Users.css";
+
 const UsersList = () => {
-const urlApi = "https://randomuser.me/api/?results=5"
-const [users, setUsers] = useState([])
-useEffect(() => {
+  const urlApi = "https://randomuser.me/api/?results=5";
+  const [users, setUsers] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(null);
+
+  useEffect(() => {
     fetch(urlApi)
-    .then(response => response.json())
-    .then(data => setUsers(data.results))
-    .catch(error => console.log('Hubo un error ' + error))
-}, [])
- 
-return(
-<div>
-    <h1>Listado: </h1>
-    
-    <ul> {users.map((user, index) => <li key={index}><div class="card">
-      <div class="content">
-        <div class="nombre">{user.name.first}</div>
+      .then(response => response.json())
+      .then(data => setUsers(data.results))
+      .catch(error => console.log('Hubo un error ' + error));
+  }, []);
+
+  const openModal = (user) => {
+    setSelectedUser(user);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div>
+      <h1>Listado: </h1>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>
+            <div className="tarjeta">
+              <div className="contenidoUsuario">
+                <img src={user.picture.thumbnail} alt="profile pic"></img>
+                <div className="nombre">{user.name.first}</div>
+              </div>
+              <button onClick={() => openModal(user)}>
+                Más información
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+      {isModalOpen && (
+        <Modal isModalOpen={isModalOpen} closeModal={closeModal} selectedUser={selectedUser} />
+      )}
+    </div>
+  );
+};
+
+const Modal = ({ isModalOpen, closeModal, selectedUser }) => {
+  return (
+    <div 
+      className={`superposicion ${isModalOpen ? 'aparecer' : 'desaparecer'}`} 
+      onAnimationEnd={(e) => {
+        if (!isModalOpen && e.animationName === 'desaparecer') {
+          closeModal();
+        }
+      }}
+    >
+      <div className="contenidoModal">
+        {selectedUser && (
+          <>
+            <h2>Información del usuario</h2>
+            <img src={selectedUser.picture.large} alt="profile pic"></img>
+            <p><b>Nombre</b>: {selectedUser.name.first} {selectedUser.name.last}</p>
+            <p><b>Email</b>: {selectedUser.email}</p>
+            <p><b>Numero de teléfono</b>: {selectedUser.phone}</p>
+            <p><b>Locacion</b>: {selectedUser.location.city}, {selectedUser.location.country}</p>
+          </>
+        )}
+        <button onClick={closeModal}>Cerrar</button>
       </div>
-        <button>
-          Más información
-        </button>
-  </div></li>)}</ul>
-</div>
-)}
-export default UsersList
+    </div>
+  );
+};
+
+export default UsersList;
